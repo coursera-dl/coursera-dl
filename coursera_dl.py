62487a0648a984132bdfedb172f87fa5f15506d1
John Lehmann <john.lehmann@gmail.com>
2012-03-25 13:28:12 -0500

    - pure python downloader added as default, wget as optional - limited paren stripping to end of filename - refactored html parsing - option to download only a section number

--- coursera_dl.py
+++ coursera_dl.py
@@ -7,34 +7,37 @@ Tested on Python 2.6.5.
 Dependencies:
 - BeautifulSoup 3
 - argparser         # sudo easy_install argparse
-- wget
 
 Other:
 - must point script at your browser's cookie file for authentication
   to coursera.org
   - Chrome users use "cookie.txt export" extension
+- wget can optionally be used
+
+If it's finding 0 sections, you probably have a bad cookies file.
+Use -l listing.html and then examine that file -- if it's the non-logged-in
+page then this is definitely your problem.
+
+Examples:
+coursera_dl.py -c cookies.txt -l listing.html -o saas --skip-download
 """
 
-import sys, os, re
-import urllib2
-import cookielib 
+import sys, os, re, string
+import urllib2, cookielib 
 import tempfile
 import subprocess
-import string
 import argparse
 from collections import namedtuple
 from BeautifulSoup import BeautifulSoup        
 
-COURSERA_INDEX = "http://www.coursera.org/%s/lecture/index"
-
-def get_index_url(className):
+def get_syllabus_url(className):
   """Return the Coursera index/syllabus URL."""
-  return COURSERA_INDEX  % className
+  return "http://class.coursera.org/%s/lecture/index" % className
 
-def get_cookies_file(cookies_file):
+def load_cookies_file(cookies_file):
   """Loads the cookies file. I am pre-pending the file with the special
-  Netscape header because the cookie loader is being very particular about this
-  string."""
+  Netscape header because the cookie loader is being very particular about 
+  this string."""
   NETSCAPE_HEADER = "# Netscape HTTP Cookie File"
   cookies = tempfile.NamedTemporaryFile()
   cookies.write(NETSCAPE_HEADER)
@@ -42,59 +45,69 @@ def get_cookies_file(cookies_file):
   cookies.flush()
   return cookies
 
+def get_opener(cookies_file):
+  """  """
+  cj = cookielib.MozillaCookieJar()
+  cookies = load_cookies_file(cookies_file)
+  cj.load(cookies.name)
+  return urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
 def get_page(url, cookies_file):
   """Download an HTML page using the cookiejar."""
-  cj = cookielib.MozillaCookieJar()
-  cookies = get_cookies_file(cookies_file)
-  cj.load(cookies.name)
-  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+  opener = get_opener(cookies_file)
   return opener.open(url).read()
+
+def get_syllabus(class_name, cookies_file, local_page=False):
+  """ Get the course listing webpage."""
+  if (not (local_page and os.path.exists(local_page))):
+    url = get_syllabus_url(class_name)
+    page = get_page(url, cookies_file)
+    print "Downloaded %s (%d bytes)" % (url, len(page))
+    # cache the page if we're in 'local' mode
+    if (local_page):
+      open(local_page, 'w').write(page)
+  else:
+    page = open(local_page).read()
+  return page
 
 def clean_filename(s):
   """Sanitize a string to be used as a filename."""
   # strip paren portions which contain trailing time length (...)
-  s = re.sub("\(.*", "", s)
+  s = re.sub("\([^\(]*$", "", s)
   s = s.strip().replace(':','-').replace(' ', '_')
   valid_chars = "-_.()%s%s" % (string.ascii_letters, string.digits)
   return ''.join(c for c in s if c in valid_chars)
 
-def parse_page(page):
-  """Parses a Coursera course listing/syllabus page."""
-  # a section corresponds to a week of classes
+def parse_syllabus(page):
+  """Parses a Coursera course listing/syllabus page. 
+  Each section is a week of classes."""
   Section = namedtuple('Section', 'name videos')
   Video = namedtuple('Video', 'name url')
   sections = []
-  videos = []
-  section_name = None
   soup = BeautifulSoup(page)
-
-  def push(sections, videos):
-    if (videos):
-      sections.append(Section(section_name, videos))
-      return []
-    return videos
-    
-  # sections and videos are interspersed
-  for tag in soup.findAll(attrs={"class":
-                         re.compile('^(list_header$|item_row )')}):
-    # section
-    if tag.string != None:
-      videos = push(sections, videos)
-      section_name = clean_filename(tag.string)
-      print section_name
-    # video
-    else:
-      assert tag.a.contents[0], "couldn't get video name"
-      vname = clean_filename(tag.a.contents[0])
+  # traverse sections
+  for stag in soup.findAll(attrs={'class':'list_header'}):
+    assert stag.string != None, "couldn't find section"
+    section_name = clean_filename(stag.string)
+    print section_name
+    videos = []
+    # traverse videos
+    for vtag in stag.parent.nextSibling.findAll('li'):
+      assert vtag.a.contents[0], "couldn't get video name"
+      vname = clean_filename(vtag.a.contents[0])
       print "  ", vname,
       # find the anchor with .mp4 reference
-      url = tag.find("a", {"href":re.compile("\.mp4")})["href"]
+      url = vtag.find('a', {"href":re.compile("\.mp4")})["href"]
       print "  ", url
       videos.append(Video(vname, url))
-  push(sections, videos)
+    sections.append(Section(section_name, videos))
+  print "Found %d sections and %d videos on this page" % \
+    (len(sections), sum((len(s.videos) for s in sections)))
+  if (not len(sections)):
+    print "Probably bad cookies file (or wrong class name)"
   return sections
 
-def download_videos(cookies_file, class_name, sections, overwrite=False, skip_download=False):
+def download_videos(wget_bin, cookies_file, class_name, sections, overwrite=False, skip_download=False, only_section_num=None):
   """Downloads videos described by sections."""
 
   def format_section(num, section):
@@ -103,36 +116,55 @@ def download_videos(cookies_file, class_name, sections, overwrite=False, skip_do
   def format_video(num, video):
     return "%02d_%s.mp4" % (num, video)
 
-  for (weeknum, (section, videos)) in enumerate(sections, 1):
-    sec = format_section(weeknum, section)
+  for (secnum, (section, videos)) in enumerate(sections, 1):
+    if only_section_num and secnum != only_section_num:
+      continue
+    sec = format_section(secnum, section)
     if not os.path.exists(sec):
       os.mkdir(sec)
     for (vidnum, (vname, url)) in enumerate(videos, 1):
       vidfn = os.path.join(sec, format_video(vidnum, vname))
       if overwrite or not os.path.exists(vidfn):
         if not skip_download: 
-          download_file(url, vidfn, cookies_file)
+          download_file(url, vidfn, cookies_file, wget_bin)
         else: 
           open(vidfn, 'w').close()  # touch
 
-def download_file(url, fn, cookies_file):
+def download_file(url, fn, cookies_file, wget_bin):
+  """Downloads file and removes current file if aborted by user."""
+  try:
+    if wget_bin:
+      download_file_wget(wget_bin, url, fn, cookies_file)
+    else:
+      download_file_nowget(url, fn, cookies_file)
+  except KeyboardInterrupt as e: 
+    print "\nKeyboard Interrupt -- Removing partial file:", fn
+    os.remove(fn)
+    sys.exit()
+
+def download_file_wget(wget_bin, url, fn, cookies_file):
   """Downloads a file using wget.  Could possibly use python to stream files to
   disk, but wget is robust and gives nice visual feedback."""
-  cmd = ["wget", url, "-O", fn, "--load-cookies", cookies_file]
+  cmd = [wget_bin, url, "-O", fn, "--load-cookies", cookies_file]
   print "Executing wget:", cmd 
   retcode = subprocess.call(cmd)
 
-def get_syllabus(class_name, cookies_file, local_page=False):
-  # get the page from the web
-  if (not local_page or not os.path.exists(local_page)):
-    page = get_page(get_index_url(class_name), cookies_file)
-    print "Downloaded course page: %d bytes" % len(page)
-    # cache the page if we're in 'local' mode
-    if (local_page):
-      open(local_page, 'w').write(page)
-  else:
-    page = open(local_page).read()
-  return page
+def download_file_nowget(url, fn, cookies_file):
+  """'Native' python downloader -- slower than wget."""
+  print "Downloading %s -> %s" % (url, fn)
+  urlfile = get_opener(cookies_file).open(url)
+  chunk_sz = 1048576
+  bytesread = 0
+  f = open(fn, "w")
+  while True:
+    data = urlfile.read(chunk_sz)
+    if not data:
+      print "."
+      break
+    f.write(data)
+    bytesread += len(data)
+    print "\r%d bytes read" % bytesread,
+    sys.stdout.flush()
 
 def parseArgs():
   parser = argparse.ArgumentParser(description='Download Coursera.org videos.')
@@ -143,6 +175,10 @@ def parseArgs():
   parser.add_argument('-c', '--cookies_file', dest='cookies_file', 
     action='store', required=True, help='full path to the cookies.txt file')
   # optional
+  parser.add_argument('-w', '--wget_bin', dest='wget_bin', 
+    action='store', default=None, help='wget binary if it should be used for downloading')
+  parser.add_argument('-s', '--section_num', dest='only_section_num', type=int,
+    action='store', help='only download this section number')
   parser.add_argument('-o', '--overwrite', dest='overwrite', 
     action='store_true', default=False, 
     help='whether existing video files should be overwritten (default: False)')
@@ -160,8 +196,8 @@ def parseArgs():
 def main():
   args = parseArgs()
   page = get_syllabus(args.class_name, args.cookies_file, args.local_page)
-  sections = parse_page(page)
-  download_videos(args.cookies_file, args.class_name, sections, args.overwrite, args.skip_download)
+  sections = parse_syllabus(page)
+  download_videos(args.wget_bin, args.cookies_file, args.class_name, sections, args.overwrite, args.skip_download, args.only_section_num)
 
 if __name__ == "__main__":
   main()
