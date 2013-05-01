8321566e49668e118cfc905d0cc30896c7b3b643
Rogério Brito <rbrito@ime.usp.br>
2013-05-01 16:01:34 -0300

    test: Add unit test for preview pages.
    There's an unfortunate fact regarding the way that we treat preview pages,
    which has another undesired side-effect.
    
    * The unfortunate fact is that we call BeautifulSoup for each preview
      video/link on the preview page.
    * The side effect is that we end up having to need network access to in the
      unittest for the previews.
    
    Actually, a consequence of the last point is that for a class that has a lot
    of lectures (say, a long course), the test takes a lot of time.

--- test/test_coursera.py
+++ test/test_coursera.py
@@ -12,6 +12,9 @@ from coursera import coursera_dl
 TEST_SYLLABUS_FILE = \
     os.path.join(os.path.dirname(__file__), "2013-nlp-syllabus.html")
 
+TEST_PREVIEW_FILE = \
+    os.path.join(os.path.dirname(__file__), "2013-preview.html")
+
 
 class TestSyllabusParsing(unittest.TestCase):
 
@@ -34,6 +37,27 @@ class TestSyllabusParsing(unittest.TestCase):
         # mp4 count
         mp4s = [res for res in resources if res[0] == "mp4"]
         self.assertEqual(len(mp4s), 102)
+
+
+    def test_parse_preview(self):
+        self.syllabus_page = open(TEST_PREVIEW_FILE).read()
+
+        sections = coursera_dl.parse_syllabus(self.syllabus_page, None)
+
+        # section count
+        self.assertEqual(len(sections), 20)
+
+        # lecture count
+        lectures = [lec for sec in sections for lec in sec[1]]
+        self.assertEqual(len(lectures), 106)
+
+        # resource count
+        resources = [res for lec in lectures for res in lec[1].items()]
+        self.assertEqual(len(resources), 106)
+
+        # mp4 count
+        mp4s = [res for res in resources if res[0] == "mp4"]
+        self.assertEqual(len(mp4s), 106)
 
 
 if __name__ == "__main__":
