fa0e9fc854689d00802b037afd018e8b43921919
Jonas De Taeye <jonasdt@fastmail.fm>
2013-06-28 21:06:35 +0200

    Add _cache path
    We may move this to another place if desirable.

--- coursera/define.py
+++ coursera/define.py
@@ -4,7 +4,12 @@
 This module defines the global constants.
 """
 
+import os
+
 AUTH_URL = 'https://www.coursera.org/maestro/api/user/login'
 CLASS_URL = 'https://class.coursera.org/{class_name}'
 AUTH_REDIRECT_URL = 'https://class.coursera.org/{class_name}' \
                     '/auth/auth_redirector?type=login&subtype=normal'
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+PATH_CACHE = os.path.join(ROOT, '_cache')
