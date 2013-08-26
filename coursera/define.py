43fb82699af234504f208be6cd0eb74e4c5f3f8c
Liu Yue <euyuil@gmail.com>
2013-08-26 13:20:48 +0800

    Changed cookie path for POSIX systems to avoid insufficient permissions.

--- coursera/define.py
+++ coursera/define.py
@@ -14,5 +14,10 @@ AUTH_REDIRECT_URL = 'https://class.coursera.org/{class_name}' \
                     '/auth/auth_redirector?type=login&subtype=normal'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PATH_CACHE = os.path.join(ROOT, '_cache')
+
+if os.name == 'posix':
+    PATH_CACHE = os.path.join('/tmp', '_coursera_dl_cache')
+else:
+    PATH_CACHE = os.path.join(ROOT, '_cache')
+
 PATH_COOKIES = os.path.join(PATH_CACHE, 'cookies')
