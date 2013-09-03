8815464a7e301f13184b8aad5fdaad52ca0b19d2
Dominik Richter <dominik.richter@gmail.com>
2013-09-03 12:48:19 +0200

    move cache folder from ROOT/_cache to tempdir determined by tempfile module
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- coursera/define.py
+++ coursera/define.py
@@ -5,6 +5,7 @@ This module defines the global constants.
 """
 
 import os
+import tempfile
 
 AUTH_URL = 'https://accounts.coursera.org/api/v1/login'
 CLASS_URL = 'https://class.coursera.org/{class_name}'
@@ -14,5 +15,5 @@ AUTH_REDIRECT_URL = 'https://class.coursera.org/{class_name}' \
                     '/auth/auth_redirector?type=login&subtype=normal'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PATH_CACHE = os.path.join(ROOT, '_cache')
+PATH_CACHE = tempfile.gettempdir()
 PATH_COOKIES = os.path.join(PATH_CACHE, 'cookies')
