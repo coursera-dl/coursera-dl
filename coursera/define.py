e6de561b11766098ecc90113fe5abf80e269fc81
Dominik Richter <dominik.richter@gmail.com>
2013-09-05 20:00:36 +0200

    create a per-user temporary directory for caching files like cookies
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- coursera/define.py
+++ coursera/define.py
@@ -15,5 +15,7 @@ AUTH_REDIRECT_URL = 'https://class.coursera.org/{class_name}' \
                     '/auth/auth_redirector?type=login&subtype=normal'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PATH_CACHE = os.path.join( tempfile.gettempdir(), '_coursera_dl_cache' )
+# define a per-user cache folder
+user = os.environ.get( "USERNAME" )
+PATH_CACHE = os.path.join( tempfile.gettempdir(), user+"_coursera_dl_cache" )
 PATH_COOKIES = os.path.join(PATH_CACHE, 'cookies')
