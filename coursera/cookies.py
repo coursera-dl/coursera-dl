83498374d6437052cbb8d66bf3c72d5a3875c504
mxamin <amin.solhizadeh@gmail.com>
2015-03-28 00:05:12 +0430

    Fix SSL Issue
    It seems Coursera blocks all connections which are made by SSLv3.
    This commit forces all HTTPS connection to use TLS v1.2.

--- coursera/cookies.py
+++ coursera/cookies.py
@@ -6,8 +6,11 @@ Cookie handling module.
 
 import logging
 import os
+import ssl
 
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
 
 from six.moves import StringIO
 from six.moves import http_cookiejar as cookielib
@@ -335,3 +338,15 @@ def get_cookies_for_class(session, class_name,
         else:
             get_authentication_cookies(session, class_name, username, password)
             write_cookies_to_cache(session.cookies, username)
+
+
+class TLSAdapter(HTTPAdapter):
+    """
+    A customized HTTP Adapter which uses TLS v1.2 for encrypted
+    connections.
+    """
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1_2)
