57500bbdaa1ad0e1a893d6d686b471b72caab822
Rogério Brito <rbrito@ime.usp.br>
2015-05-06 19:24:33 -0300

    setup.py: Prepare for release 0.0.3.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='coursera',
-    version='0.0.2',
+    version='0.0.3',
     maintainer='Rogério Theodoro de Brito',
     maintainer_email='rbrito@ime.usp.br',
 
