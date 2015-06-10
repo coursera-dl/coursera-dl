0cc41927c438f611622556092a72d8cee0c8f4f5
Rogério Brito <rbrito@ime.usp.br>
2015-06-10 17:28:27 -0300

    coursera/cookies: Try to work around import errors with requests/urrlib3.
    Ubuntu 14.04's requests packages behave differently from the upstream
    version of requests.
    
    The upstream version of requests includes an embedded copy of urllib3 (and
    other things, like its own trusted root certificates), but, by Debian's
    guidelines, convenience copies of libraries are discouraged and generally
    removed.
    
    Current versions of Debian's requests packages take care of transparently
    linking the separately downloaded requests with urllib3 with glue code that
    makes them work as well as with upstream's version of requests.
    
    Unfortunately, given some reports from users (see issue #331), the version
    of requests that comes with Ubuntu 14.04 doesn't seem to have that glue code
    working as we expect.
    
    This commit tries to cope with that, by trying to import urllib3 from more
    places to see if we can get farther with the script, instead of bombing with
    an import error.
    
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- coursera/cookies.py
+++ coursera/cookies.py
@@ -10,7 +10,12 @@ import ssl
 
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.poolmanager import PoolManager
+
+try:
+    from requests.packages.urllib3.poolmanager import PoolManager
+except ImportError:
+    from urllib3.poolmanager import PoolManager
+
 
 from six.moves import StringIO
 from six.moves import http_cookiejar as cookielib
