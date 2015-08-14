212c1db6bf5b92cf19fff57448ba81453664e2f7
Rogério Brito <rbrito@ime.usp.br>
2015-08-14 00:12:11 -0300

    setup.py: Refactor script to be more resilient to missing README.rst
    While we are at it, we also break some of code in smaller pieces for
    readability purposes.
    
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- setup.py
+++ setup.py
@@ -8,6 +8,45 @@ from setuptools import setup
 #   ./venv/bin/pip install --editable .[dev]  # with dev requirements, too
 
 
+def read_file(filename, alt=None):
+    '''
+    Read the contents of filename or give an alternative result instead.
+    '''
+    lines = None
+    try:
+        with open(filename) as f:
+            lines = f.readlines()
+    except IOError:
+        lines = [] if alt is None else alt
+    return lines
+
+
+long_description = read_file(
+    'README.rst',
+    'Generate README.rst from README.md via pandoc!\n\nExample: '
+    'pandoc --from=markdown_github --to=rst --output=README.rst README.md'
+)
+install_requires = read_file('requirements.txt')
+install_dev_requires = read_file('requirements-dev.txt')
+
+trove_classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Programming Language :: Python',
+    'Topic :: Education',
+]
+
 setup(
     name='coursera',
     version='0.0.3',
@@ -17,31 +56,15 @@ setup(
     license='LGPL',
     url='https://github.com/coursera-dl/coursera',
 
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=install_requires,
     extras_require=dict(
-        dev=open('requirements-dev.txt').readlines()
+        dev=install_dev_requires
     ),
 
     description='Script for downloading Coursera.org videos and naming them.',
-    long_description=open('README.rst', 'r').read(),
+    long_description=long_description,
     keywords=['coursera', 'download', 'education', 'MOOCs', 'video'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Programming Language :: Python',
-        'Topic :: Education',
-    ],
+    classifiers=trove_classifiers,
 
     packages=["coursera"],
     entry_points=dict(
