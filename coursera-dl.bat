c9b69664bffef5c3a72d483544aae35ab75d546d
Victor Westmann <victor.westmann@gmail.com>
2015-09-11 01:05:09 -0300

    Fixed the bug in Windows of breaking the code when running path with spaces.

--- coursera-dl.bat
+++ coursera-dl.bat
@@ -6,4 +6,9 @@ rem Usage: see "Running the script" section of the README for sample commands.
 
 rem Run "coursera-dl" in the current folder as working directory (where "%~dp0" is the
 rem location of this .bat file) via Python, passing all user-specified arguments ("%*")
-python %~dp0\coursera-dl %*
+
+rem This command does not work on Windows paths with spaces
+rem python %~dp0\coursera-dl %*
+
+rem Changed to this command. That does work with spaces in path. YAY! (Victor Westmann 2015-11-09 1:01 AM)
+python coursera-dl %*
