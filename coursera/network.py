b222d042305ee7ae4e468583bd1de3ca60c0c089
Yuri Bochkarev <baltazar.bz@gmail.com>
2016-06-09 23:24:20 +0300

    Refactor common pattern into get_page_json utility function

--- coursera/network.py
+++ coursera/network.py
@@ -23,6 +23,7 @@ def get_page(session, url):
 
     return r.text
 
+
 def get_page_and_url(session, url):
     """
     Download an HTML page using the requests session and return
@@ -38,3 +39,24 @@ def get_page_and_url(session, url):
         raise
 
     return r.text, r.url
+
+
+def get_page_json(session, url, **kwargs):
+    """
+    Download page and parse it as JSON. This is a shorthand for common
+    operation.
+
+    @param session: Requests session.
+    @type session: requests.Session
+
+    @param url: URL pattern with optional keywords to format.
+    @type url: str
+
+    @param kwargs: Arguments to `url` pattern.
+
+    @return: Parsed JSON of the request page.
+    @rtype: dict
+    """
+    url = url.format(**kwargs)
+    page = get_page(session, url)
+    return json.loads(page)
