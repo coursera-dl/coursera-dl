af700279226fd57a73e13b0b5ea375414de9d174
Yuri Bochkarev <baltazar.bz@gmail.com>
2016-07-11 17:22:28 +0300

    Add encoding to slurp_fixture (attempt to fix test failures on Windows Python3.3)

--- coursera/test/utils.py
+++ coursera/test/utils.py
@@ -6,4 +6,4 @@ import os
 
 def slurp_fixture(path):
     return open(os.path.join(os.path.dirname(__file__),
-                             "fixtures", path)).read()
+                             "fixtures", path), encoding='utf8').read()
