d72644258d733643f0a8808631737c41c8b3a031
Yuri Bochkarev <baltazar.bz@gmail.com>
2016-07-13 19:02:11 +0300

    Extract module/section/lecture iteration logic into separate iterator classes
    This hopefully clarifies the download process. The remaining
    method _handle_resource needs more simplification.

--- coursera/workflow.py
+++ coursera/workflow.py
@@ -15,6 +15,87 @@ from .filter import find_resources_to_get, skip_format_url
 from .define import IN_MEMORY_MARKER
 
 
+def _iter_modules(modules, class_name, path, ignored_formats, args):
+    """
+    This huge function generates a hierarchy with hopefully more
+    clear structure of modules/sections/lectures.
+    """
+    file_formats = args.file_formats
+    lecture_filter = args.lecture_filter
+    resource_filter = args.resource_filter
+    section_filter = args.section_filter
+    verbose_dirs = args.verbose_dirs
+    combined_section_lectures_nums = args.combined_section_lectures_nums
+
+    class IterModule(object):
+        def __init__(self, index, module):
+            self.index = index
+            self.name = '%02d_%s' % (index + 1, module[0])
+            self._module = module
+
+        @property
+        def sections(self):
+            sections = self._module[1]
+            for (secnum, (section, lectures)) in enumerate(sections):
+                if section_filter and not re.search(section_filter, section):
+                    logging.debug('Skipping b/c of sf: %s %s',
+                                  section_filter, section)
+                    continue
+
+                yield IterSection(self, secnum, section, lectures)
+
+    class IterSection(object):
+        def __init__(self, module_iter, secnum, section, lectures):
+            self.index = secnum
+            self.dir = os.path.join(
+                path, class_name, module_iter.name,
+                format_section(secnum + 1, section,
+                               class_name, verbose_dirs))
+            self._lectures = lectures
+
+        @property
+        def lectures(self):
+            for (lecnum, (lecname, lecture)) in enumerate(self._lectures):
+                if lecture_filter and not re.search(lecture_filter, lecname):
+                    logging.debug('Skipping b/c of lf: %s %s',
+                                  lecture_filter, lecname)
+                    continue
+
+                yield IterLecture(self, lecnum, lecname, lecture)
+
+    class IterLecture(object):
+        def __init__(self, section_iter, lecnum, lecname, lecture):
+            self.index = lecnum
+            self.name = lecname
+            self._lecture = lecture
+            self._section_iter = section_iter
+
+        def filename(self, fmt, title):
+            lecture_filename = get_lecture_filename(
+                combined_section_lectures_nums,
+                self._section_iter.dir, self._section_iter.index,
+                self.index, self.name, title, fmt)
+            return lecture_filename
+
+        @property
+        def resources(self):
+            resources_to_get = find_resources_to_get(
+                self._lecture, file_formats, resource_filter,
+                ignored_formats)
+
+            for fmt, url, title in resources_to_get:
+                yield IterResource(fmt, url, title)
+
+    class IterResource(object):
+        def __init__(self, fmt, url, title):
+            self.fmt = fmt
+            self.url = url
+            self.title = title
+
+    for index, module in enumerate(modules):
+        yield IterModule(index, module)
+
+
 class CourseDownloader(object):
     __metaclass__ = abc.ABCMeta
 
@@ -46,122 +127,44 @@ class CourseraDownloader(CourseDownloader):
         self.skipped_urls = [] if disable_url_skipping else None
         self.failed_urls = []
 
-        self._last_update = -1
-
     def download_modules(self, modules):
         completed = True
-        for idx, module in enumerate(modules):
-            module_name = '%02d_%s' % (idx + 1, module[0])
-            sections = module[1]
-            result = self._download_sections(module_name, sections)
-            completed = completed and result
+        modules = _iter_modules(
+            modules, self._class_name, self._path,
+            self._ignored_formats, self._args)
 
-        # Wait for all downloads to complete
-        self._downloader.join()
+        for module in modules:
+            last_update = -1
+            for section in module.sections:
+                if not os.path.exists(section.dir):
+                    mkdir_p(normalize_path(section.dir))
 
-        # Iterate over modules again to apply playlist creation/other hooks
-        # after download has finished
-        self._apply_postprocessing(modules)
-
-        return completed
-
-    def _apply_postprocessing(self, modules):
-        """
-        Apply postprocessing hooks to downloaded modules.
-        """
-        section_filter = self._args.section_filter
-        hooks = self._args.hooks
-        playlist = self._args.playlist
-        verbose_dirs = self._args.verbose_dirs
-
-        for idx, module in enumerate(modules):
-            module_name = '%02d_%s' % (idx + 1, module[0])
-            sections = module[1]
-
-            for (secnum, (section, lectures)) in enumerate(sections):
-                if section_filter and not re.search(section_filter, section):
-                    continue
-
-                section_dir = os.path.join(
-                    self._path, self._class_name, module_name,
-                    format_section(secnum + 1, section,
-                                   self._class_name, verbose_dirs))
+                for lecture in section.lectures:
+                    for resource in lecture.resources:
+                        lecture_filename = normalize_path(
+                            lecture.filename(resource.fmt, resource.title))
+                        last_update = self._handle_resource(
+                            resource.url, resource.fmt, lecture_filename,
+                            self._download_completion_handler, last_update)
 
                 # After fetching resources, create a playlist in M3U format with the
                 # videos downloaded.
-                if playlist:
-                    create_m3u_playlist(section_dir)
+                if self._args.playlist:
+                    create_m3u_playlist(section.dir)
 
-                if hooks:
-                    original_dir = os.getcwd()
-                    for hook in hooks:
-                        logging.info('Running hook %s for section %s.', hook, section_dir)
-                        os.chdir(section_dir)
-                        subprocess.call(hook)
-                    os.chdir(original_dir)
+                if self._args.hooks:
+                    self._run_hooks(section, self._args.hooks)
 
-    def _download_sections(self, module_name, sections):
-        """
-        Download lecture resources described by sections.
+            # if we haven't updated any files in 1 month, we're probably
+            # done with this course
+            completed = completed and is_course_complete(last_update)
 
-        Returns True if the class appears completed, False otherwise.
-        """
-        self._last_update = -1
-
-        section_filter = self._args.section_filter
-        verbose_dirs = self._args.verbose_dirs
-
-        for (secnum, (section, lectures)) in enumerate(sections):
-            if section_filter and not re.search(section_filter, section):
-                logging.debug('Skipping b/c of sf: %s %s', section_filter,
-                              section)
-                continue
-
-            section_dir = os.path.join(
-                self._path, self._class_name, module_name,
-                format_section(secnum + 1, section,
-                               self._class_name, verbose_dirs))
-
-            self._download_lectures(lectures, secnum, section_dir)
-
-        # if we haven't updated any files in 1 month, we're probably
-        # done with this course
-        is_complete = is_course_complete(self._last_update)
-        if is_complete:
+        if completed:
             logging.info('COURSE PROBABLY COMPLETE: ' + self._class_name)
 
-        return is_complete
-
-    def _download_lectures(self, lectures, secnum, section_dir):
-        lecture_filter = self._args.lecture_filter
-        file_formats = self._args.file_formats
-        resource_filter = self._args.resource_filter
-        combined_section_lectures_nums = self._args.combined_section_lectures_nums
-
-        for (lecnum, (lecname, lecture)) in enumerate(lectures):
-            if lecture_filter and not re.search(lecture_filter,
-                                                lecname):
-                logging.debug('Skipping b/c of lf: %s %s', lecture_filter,
-                              lecname)
-                continue
-
-            if not os.path.exists(section_dir):
-                mkdir_p(normalize_path(section_dir))
-
-            resources_to_get = find_resources_to_get(lecture,
-                                                     file_formats,
-                                                     resource_filter,
-                                                     self._ignored_formats)
-
-            # write lecture resources
-            for fmt, url, title in resources_to_get:
-                lecture_filename = get_lecture_filename(
-                    combined_section_lectures_nums,
-                    section_dir, secnum, lecnum, lecname, title, fmt)
-
-                lecture_filename = normalize_path(lecture_filename)
-                self._handle_resource(url, fmt, lecture_filename,
-                                      self._download_completion_handler)
+        # Wait for all downloads to complete
+        self._downloader.join()
+        return completed
 
     def _download_completion_handler(self, url, result):
         if isinstance(result, requests.exceptions.RequestException):
@@ -169,22 +172,32 @@ class CourseraDownloader(CourseDownloader):
                           'downloading URL %s: %s', url, str(result))
             self.failed_urls.append(url)
         elif isinstance(result, Exception):
-            logging.error('Unknown exception occurred: %s' % result)
+            logging.error('Unknown exception occurred: %s', result)
             self.failed_urls.append(url)
 
-    def _handle_resource(self, url, fmt, lecture_filename, callback):
+    def _handle_resource(self, url, fmt, lecture_filename, callback, last_update):
         """
         Handle resource. This function builds up resource file name and
         downloads it if necessary.
 
-        @param fmt: Format of the resource (pdf, csv, etc)
-        @type fmt: str
-
         @param url: URL of the resource.
         @type url: str
 
+        @param fmt: Format of the resource (pdf, csv, etc)
+        @type fmt: str
+
+        @param lecture_filename: File name of the lecture.
+        @type lecture_filename: str
+
+        @param callback: Callback that will be called when file has been
+            downloaded. It will be called even if exception occurred.
+        @type callback: callable(url, result) where result may be Exception
+
+        @param last_update: Timestamp of the newest file so far.
+        @type last_update: int
+
         @return: Updated latest mtime.
-        @rtype: timestamp
+        @rtype: int
         """
         overwrite = self._args.overwrite
         resume = self._args.resume
@@ -206,10 +219,20 @@ class CourseraDownloader(CourseDownloader):
                         self._downloader.download(callback, url, lecture_filename, resume=resume)
             else:
                 open(lecture_filename, 'w').close()  # touch
-            self._last_update = time.time()
+            last_update = time.time()
         else:
             logging.info('%s already downloaded', lecture_filename)
             # if this file hasn't been modified in a long time,
             # record that time
-            self._last_update = max(self._last_update,
-                                    os.path.getmtime(lecture_filename))
+            last_update = max(last_update,
+                              os.path.getmtime(lecture_filename))
+        return last_update
+
+    def _run_hooks(self, section, hooks):
+        original_dir = os.getcwd()
+        for hook in hooks:
+            logging.info('Running hook %s for section %s.',
+                         hook, section.dir)
+            os.chdir(section.dir)
+            subprocess.call(hook)
+        os.chdir(original_dir)
