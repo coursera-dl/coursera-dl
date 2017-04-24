701a7be4db1421f4cca39d36bd2f2ae245a7c4be
Neven Miculinic <neven.miculinic@gmail.com>
2017-04-24 16:52:04 +0200

    Added create all playlists
    Playlist file before generated only .m3u playlist where mp4 files exist
    in folder. However many courses have multi level deep folder structure,
    often yielding one video per playlist. Single course week has multiple
    folders underneath it.
    
    This diff creates additional playlists for every folder in hierarchy,
    as long as there's mp4 file under its tree. This enables having single
    playlist per week, course, or even whole course collection.
    
    ---
    
    Test plan:
    
    * Tested on local PC with multiple coursea courses
    * Works when starting playing in video player
    * Inspected generated .m3u files for sanity checks

--- coursera/playlist.py
+++ coursera/playlist.py
@@ -12,15 +12,26 @@ def create_m3u_playlist(section_dir):
     """
     path_to_return = os.getcwd()
 
-    for (_path, subdirs, files) in os.walk(section_dir):
+    for (_path, subdirs, files) in os.walk(section_dir, topdown=False):
         os.chdir(_path)
         globbed_videos = sorted(glob.glob("*.mp4"))
         m3u_name = os.path.split(_path)[1] + ".m3u"
+
+        if not len(globbed_videos):
+            for subdir in subdirs:
+                for fname in os.listdir(subdir):
+                    if fname.endswith(".m3u"):
+                        with open(os.path.join(subdir, fname)) as f:
+                            for line in f.readlines():
+                                globbed_videos.append(os.path.join(subdir, line.strip()))
 
         if len(globbed_videos):
             with open(m3u_name, "w") as m3u:
                 for video in globbed_videos:
                     m3u.write(video + "\n")
-            os.chdir(path_to_return)
+
     os.chdir(path_to_return)
 
+
+if __name__ == "__main__":
+    create_m3u_playlist(os.getcwd())
