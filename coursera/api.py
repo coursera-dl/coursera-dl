9c2595f5dcfd7cc8f6a2be90debdc3bcbfcd147a
Abdallah Samman <sammanabdallah@gmail.com>
2018-11-13 22:38:24 +0200

    #692 Fix Definition Value

--- coursera/api.py
+++ coursera/api.py
@@ -1475,12 +1475,19 @@ class CourseraOnDemand(object):
         for element in dom['elements']:
             # There is only one section with Instructions
             if 'introduction' in element['instructions']:
-                result.append(element['instructions']
-                              ['introduction']['definition']['value'])
+                try:
+                    result.append(element['instructions']
+                                  ['introduction']['definition']['value'])
+                except Exception as e:
+                    result.append(element['instructions']
+                                  ['introduction']['definition'])
 
             # But there may be multiple sections in Sections
             for section in element['instructions'].get('sections', []):
-                section_value = section['content']['definition']['value']
+                try:
+                    section_value = section['content']['definition']['value']
+                except Exception as e:
+                    section_value = section['content']['definition']
                 section_title = section.get('title')
                 if section_title is not None:
                     # If section title is present, put it in the beginning of
